95afe21dbaa3ee7edf8a778fd637c5eb0579488f
bon0502 <bonkkuma@gmail.com>
2026-06-17 17:34:29 +0900

    feat: Rails プロジェクトの初期セットアップ

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,7 @@
+// app/javascript/controllers/index.js
+import { application } from "./application"
+
+// 今後 Stimulus コントローラーを作成したら、ここに追加していく
+// 例:
+// import HelloController from "./hello_controller"
+// application.register("hello", HelloController)
